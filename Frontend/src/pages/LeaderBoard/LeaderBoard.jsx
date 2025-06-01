93d700a59f354e22c8fcb81142e3e87081719ce7
doaamohamed88 <doaatawfik82001@gmail.com>
2025-06-01 15:38:08 +0300

    add page header$change bg

--- Frontend/src/pages/LeaderBoard/LeaderBoard.jsx
+++ Frontend/src/pages/LeaderBoard/LeaderBoard.jsx
@@ -12,21 +12,26 @@ export default function LeaderBoard() {
   };
   return (
     <>
-      <div
-        className={`${styles.tableContainer}
+      <div className="leaderboard-container">
+        <div className={styles.header}>
+          <h1>Leader Board</h1>
+          <p>Check out the top teams and their scores!</p>
+        </div>
+        <div
+          className={`${styles.tableContainer}
           ${styles.rankCardContainer}
         `}
-      >
-       <Podium />
-
-      </div>
-      <div className={styles.tableContainer} >
-        <div className={styles.tableHeaderInfo}>
-          <h3 style={{ textAlign: "center" }}>team 1</h3>
-          <span>Total score: {totalScore}</span>
+        >
+          <Podium />
         </div>
+        <div className={styles.tableContainer}>
+          <div className={styles.tableHeaderInfo}>
+            <h3 style={{ textAlign: "center" }}>team 1</h3>
+            <span>Total score: {totalScore}</span>
+          </div>
 
-        <TeamTable calculateTotalScore={calculateTotalScore}/>
+          <TeamTable calculateTotalScore={calculateTotalScore} />
+        </div>
       </div>
     </>
   );
